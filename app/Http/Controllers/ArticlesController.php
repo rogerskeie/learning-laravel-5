21c4e945f2072e09054c61f8333f665a32b99cf0
Roger Skeie <rogerskeie@gmail.com>
2015-10-17 16:36:59 +0000

    Added create article form.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,22 +4,59 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Article;
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Request;
 
+/**
+ * Class ArticlesController
+ * @package App\Http\Controllers
+ */
 class ArticlesController extends Controller {
 
+    /**
+     * Show all articles
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
 
         return view('articles.index', compact('articles'));
 	}
 
+    /**
+     * Show specific article
+     * @param $id
+     * @return \Illuminate\View\View
+     */
     public function show($id)
     {
         $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+    }
+
+    /**
+     * Show view for creating a new article
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store the new article in the database
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store()
+    {
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+
+        Article::create($input);
+
+        return redirect('articles');
     }
 
 }
