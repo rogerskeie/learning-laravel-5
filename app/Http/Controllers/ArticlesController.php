304cffd9ec549d9c18d329ac7f9284c6eaf488c7
unknown <rogerskeie@gmail.com>
2015-10-29 20:28:05 +0100

    Syncing the article tags when editing an article.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Auth;
  */
 class ArticlesController extends Controller {
 
+    /**
+     * Create a new articles controller instance.
+     */
     public function __construct()
     {
         $this->middleware('auth', ['only' => 'create']);
@@ -64,9 +67,7 @@ class ArticlesController extends Controller {
      */
     public function store(ArticleRequest $request)
     {
-        $article = Auth::user()->articles()->create($request->all());
-
-        $article->tags()->attach($request->input('tag_list'));
+        $this->createArticle($request);
 
         flash()->overlay('Your article has been successfully created!', 'Good Job');
 
@@ -97,7 +98,35 @@ class ArticlesController extends Controller {
     {
         $article->update($request->all());
 
+        $this->syncTags($article, $request->input('tag_list'));
+
         return redirect('articles');
+    }
+
+    /**
+     * Sync up the list of tags in the database.
+     *
+     * @param Article $article
+     * @param array $tags
+     */
+    private function syncTags(Article $article, array $tags)
+    {
+        $article->tags()->sync($tags);
+    }
+
+    /**
+     * Save a new article
+     *
+     * @param ArticleRequest $request
+     * @return mixed
+     */
+    private function createArticle(ArticleRequest $request)
+    {
+        $article = Auth::user()->articles()->create($request->all());
+
+        $this->syncTags($article, $request->input('tag_list'));
+
+        return $article;
     }
 
 }
